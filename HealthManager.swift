d24ed205125811c0ee9727401c714e6440baa59b
Joseph Raso <joeraso@seas.1056.apng.wireless-pennnet.upenn.edu>
2016-09-09 21:39:56 -0400

    Adding healthmanager

--- HealthManager.swift
+++ HealthManager.swift
@@ -14,22 +14,15 @@ let healthKitStore:HKHealthStore = HKHealthStore()
 func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
 {
     // 1. Set the types you want to read from HK Store
-    let healthKitTypesToRead = Set(arrayLiteral:[
+    let healthKitTypesToRead : Set<HKCharacteristicType> = [
         HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth),
         HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBloodType),
         HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex),
         HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass),
         HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight),
         HKObjectType.workoutType()
-        ])
+        ]
     
-    // 2. Set the types you want to write to HK Store
-    let healthKitTypesToWrite = Set(arrayLiteral:[
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning),
-        HKQuantityType.workoutType()
-        ])
     
     // 3. If the store is not available (for instance, iPad) return an error and don't go on.
     if !HKHealthStore.isHealthDataAvailable()
@@ -43,7 +36,7 @@ func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
     }
     
     // 4.  Request HealthKit authorization
-    healthKitStore.requestAuthorizationToShareTypes(healthKitTypesToWrite, readTypes: healthKitTypesToRead) { (success, error) -> Void in
+    healthKitStore.requestAuthorizationToShareTypes([], readTypes: healthKitTypesToRead) { (success, error) -> Void in
         
         if( completion != nil )
         {
