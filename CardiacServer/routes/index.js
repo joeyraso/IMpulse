f3580ba4572ad8e8e03efa262ee289895c7285fe
Joseph Raso <joeraso@seas01255.wlan.wireless-pennnet.upenn.edu>
2016-09-10 13:49:49 -0400

    FDSAFD

--- CardiacServer/routes/index.js
+++ CardiacServer/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/message', function(req, res, next) {
+	res.send('sent message\n');
 });
 
 module.exports = router;
