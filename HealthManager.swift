01c5dc91eda3739b691abaf3ba31e2af6f86ab62
bethanydavis <davisbet@seas.upenn.edu>
2016-09-09 21:36:58 -0400

    Update healthKitTypesToRead and eliminate TypesToWrite

--- HealthManager.swift
+++ HealthManager.swift
@@ -13,25 +13,10 @@ let healthKitStore:HKHealthStore = HKHealthStore()
 
 func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
 {
-    // 1. Set the types you want to read from HK Store
-    let healthKitTypesToRead = Set(arrayLiteral:[
-        HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth),
-        HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBloodType),
-        HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight),
-        HKObjectType.workoutType()
-        ])
+    // Set the types you want to read from HK Store
+    let healthKitTypesToRead : Set<HKObjectType> = [HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)!]
     
-    // 2. Set the types you want to write to HK Store
-    let healthKitTypesToWrite = Set(arrayLiteral:[
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned),
-        HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning),
-        HKQuantityType.workoutType()
-        ])
-    
-    // 3. If the store is not available (for instance, iPad) return an error and don't go on.
+    // If the store is not available (for instance, iPad) return an error and don't go on.
     if !HKHealthStore.isHealthDataAvailable()
     {
         let error = NSError(domain: "com.raywenderlich.tutorials.healthkit", code: 2, userInfo: [NSLocalizedDescriptionKey:"HealthKit is not available in this Device"])
@@ -42,8 +27,8 @@ func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
         return;
     }
     
-    // 4.  Request HealthKit authorization
-    healthKitStore.requestAuthorizationToShareTypes(healthKitTypesToWrite, readTypes: healthKitTypesToRead) { (success, error) -> Void in
+    // Request HealthKit authorization
+    healthKitStore.requestAuthorizationToShareTypes(Set(), readTypes: healthKitTypesToRead) { (success, error) -> Void in
         
         if( completion != nil )
         {
